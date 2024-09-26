59268393944a3301c8e0608a0db8826606645922
Luka Mucko <luka.mucko@gmail.com>
2024-09-26 18:21:09 +0200

    MLP and SimpleNet

--- sde_todo/network.py
+++ sde_todo/network.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 from jaxtyping import Array, Int, Float
+import math
 
 class PositionalEncoding(nn.Module):
 
@@ -14,6 +15,10 @@ class PositionalEncoding(nn.Module):
             t_channel: number of modulation channel
         """
         super().__init__()
+        self.t_channel = t_channel
+        
+        if t_channel % 2 !=0:
+            raise ValueError("t_channel must be an even number")
 
     def forward(self, t: Float):
         """
@@ -25,16 +30,23 @@ class PositionalEncoding(nn.Module):
         Returns:
             emb: time embedding
         """
-        emb = None
+        if t.dim() == 0:
+            t = t.unsqueeze(0)
+        if t.dim() == 1:
+            t = t.unsqueeze(-1)
+        emb = torch.zeros(t.shape[0], self.t_channel, device=t.device)
+        
+        div_term = torch.exp(torch.arange(0, self.t_channel, 2, device=t.device) * (-math.log(10000.0) / self.t_channel))
+        emb[:, 0::2] = torch.sin(t * div_term)
+        emb[:, 1::2] = torch.cos(t * div_term)
         return emb
-
 
 class MLP(nn.Module):
 
     def __init__(self,
                  in_dim: Int,
                  out_dim: Int,
-                 hid_shapes: Int[Array, "foo bar"]):
+                 hid_shapes: Int[Array, "h1 ... hn"]):
         '''
         (TODO) Build simple MLP
 
@@ -44,7 +56,14 @@ class MLP(nn.Module):
             hid_shapes: array of hidden layers' dimension
         '''
         super().__init__()
-        self.model = None
+        modules = [nn.Linear(in_dim, hid_shapes[0]), nn.ReLU()]
+        
+        for i in range(1, len(hid_shapes)):
+            modules.append(nn.Linear(hid_shapes[i-1], hid_shapes[i]))
+            if i != len(hid_shapes):
+                modules.append(nn.ReLU())
+        modules.append(nn.Linear(hid_shapes[-1], out_dim))
+        self.model = nn.Sequential(*modules)
 
     def forward(self, x: Array):
         return self.model(x)
@@ -55,9 +74,10 @@ class SimpleNet(nn.Module):
 
     def __init__(self,
                  in_dim: Int,
-                 enc_shapes: Int[Array],
-                 dec_shapes: Int[Array],
-                 z_dim: Int):
+                 enc_shapes: Int[Array, "h1 ... hn"],
+                 dec_shapes: Int[Array, "h1 ... hn"],
+                 z_dim: Int,
+                 t_channel: Int=64):
         super().__init__()
         '''
         (TODO) Build Score Estimation network.
@@ -74,6 +94,9 @@ class SimpleNet(nn.Module):
             dec_shapes: array of dimensions of decoder
             z_dim: output dimension of encoder
         '''
+        self.embedder = PositionalEncoding(t_channel)
+        self.encoder = MLP(in_dim + t_channel, z_dim, enc_shapes)
+        self.decoder = MLP(z_dim, in_dim, enc_shapes)
 
     def forward(self, t: Array, x: Array):
         '''
@@ -86,5 +109,9 @@ class SimpleNet(nn.Module):
             t: the time that the forward diffusion has been running
             x: the noisy data after t period diffusion
         '''
-        s = None
+        t_embedd = self.embedder(t)
+        x_in = torch.cat([x, t_embedd], axis=-1)
+        
+        z = self.encoder(x_in)
+        s = self.decoder(z)
         return s
