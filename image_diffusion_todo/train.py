b35e5ccb839d97feea8a23ec0e58b4c577d213f4
Luka Mucko <luka.mucko@gmail.com>
2024-10-19 19:12:25 +0200

    train script and nb with generated imgs and inpainting

--- image_diffusion_todo/train.py
+++ image_diffusion_todo/train.py
@@ -1,158 +1,67 @@
-import argparse
-import json
-from datetime import datetime
-from pathlib import Path
-
-import matplotlib
-import matplotlib.pyplot as plt
 import torch
-from dataset import AFHQDataModule, get_data_iterator, tensor_to_pil_image
-from dotmap import DotMap
-from ddpm import DiffusionModule
-from network import UNet, EMA
-from pytorch_lightning import seed_everything
-from scheduler import DDIMScheduler, DDPMScheduler
-from torchvision.transforms.functional import to_pil_image
-from tqdm import tqdm
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim.lr_scheduler import ExponentialLR
+from UNet import UNet
+from scheduler import DDPMScheduler, DDIMScheduler
+from dataset import get_dataloader
+import tqdm.notebook as tqdm
+import argparse
+from torch_ema import ExponentialMovingAverage
 import os
 
-matplotlib.use("Agg")
+device = "cuda"
 
-def get_current_time():
-    now = datetime.now().strftime("%m-%d-%H%M%S")
-    return now
-
-
-def main(args):
-    """config"""
-    config = DotMap()
-    config.update(vars(args))
-    config.device = f"cuda:{args.gpu}"
-
-    now = get_current_time()
-    
-    os.makedirs("results", exist_ok=True)
-    save_dir = Path(f"results/diffusion-{now}")
-    save_dir.mkdir(exist_ok=True)
-    print(f"save_dir: {save_dir}")
-
-    #seed_everything(config.seed)
-
-    with open(save_dir / "config.json", "w") as f:
-        json.dump(config, f, indent=2)
-    """######"""
-
-    image_resolution = config.image_resolution
-    ds_module = AFHQDataModule(
-        "./data",
-        batch_size=config.batch_size,
-        num_workers=4,
-        max_num_images_per_cat=config.max_num_images_per_cat,
-        image_resolution=image_resolution
-    )
-
-    train_dl = ds_module.train_dataloader()
-    train_it = get_data_iterator(train_dl)
-
-    var_scheduler = DDPMScheduler(
-        config.num_diffusion_train_timesteps,
-        beta_1=config.beta_1,
-        beta_T=config.beta_T,
-        mode="linear",
-    )
-    if isinstance(var_scheduler, DDIMScheduler):
-        var_scheduler.set_timesteps(20)  # 20 steps are enough in the case of DDIM.
-
-    network = UNet(
-        T=config.num_diffusion_train_timesteps,
-        image_resolution=image_resolution,
-        ch=64,
-        ch_mult=[1, 2, 2, 4],
-        attn=[False, False, True, True],
-        num_res_blocks=4,
-        dropout=0.1,
-        use_cfg=False,
-        cfg_dropout=0.1,
-        num_classes=getattr(ds_module, "num_classes", None),
-    )
-
-    ddpm = DiffusionModule(network, var_scheduler)
-    ddpm = ddpm.to(config.device)
-
-    optimizer = torch.optim.Adam(ddpm.network.parameters(), lr=2e-4)
-    scheduler = torch.optim.lr_scheduler.LambdaLR(
-        optimizer, lr_lambda=lambda t: min((t + 1) / config.warmup_steps, 1.0)
-    )
-    
-    ema = EMA(ddpm.network, decay=0.999)
-
-    step = 0
+def train(pipeline, dataloader, num_epochs=100, save_path="results/train1/"):
+    model = pipeline.model
+    optimizer = torch.optim.Adam(model.parameters(), lr=2e-4)
+    ema = ExponentialMovingAverage(model.parameters(), decay=0.995)
     losses = []
-    with tqdm(initial=step, total=config.train_num_steps) as pbar:
-        while step < config.train_num_steps:
-            if step % config.log_interval == 0:
-                ddpm.eval()
-                ema.apply_shadow()  
-                plt.plot(losses)
-                plt.yscale('log')
-                plt.savefig(f"{save_dir}/loss.png")
-                plt.close()
+    pipeline.train()
+    epoch_bar = tqdm.tqdm(range(num_epochs), desc="Epochs", leave=True)
+    for epoch in epoch_bar:
+        epoch_losses = []
+        batch_bar = tqdm.tqdm(dataloader, desc="Batches", leave=False)
+        for batch in batch_bar:
+                images = batch["image"].to(device)
+                labels = batch["label"].to(device)
+                optimizer.zero_grad()
+                
+                loss = pipeline.get_loss(images, labels)
+                epoch_losses.append(loss.item())
+                loss.backward()
+                optimizer.step()
+                
+                ema.update()
+                
+                avg_loss = sum(epoch_losses[-10:]) / min(len(epoch_losses), 10)
+                batch_bar.set_postfix({"avg_loss": avg_loss})
+                
+        avg_loss_epoch = sum(epoch_losses) / len(epoch_losses)
+        losses.append(avg_loss_epoch)
+        epoch_bar.set_postfix({"epoch_avg_loss": avg_loss_epoch})
+    
+    if not os.path.exists(save_path):
+        os.makedirs(save_path, exists_ok=True)
+    pipeline.save_model(save_path+"model.pt")
 
-                samples = ddpm.sample(4, return_traj=False)
-                pil_images = tensor_to_pil_image(samples)
-                for i, img in enumerate(pil_images):
-                    img.save(save_dir / f"step={step}-{i}.png")
-
-                ddpm.save(f"{save_dir}/last.ckpt")
-                ema.restore()
-                ddpm.train()
-
-            img, label = next(train_it)
-            img, label = img.to(config.device), label.to(config.device)
-            loss = ddpm.get_loss(img, class_label=label)
-            pbar.set_description(f"Loss: {loss.item():.4f}")
-
-            optimizer.zero_grad()
-            loss.backward()            
-            optimizer.step()
-            scheduler.step()
-            losses.append(loss.item())
-
-            ema.update(ddpm.network)
-
-            step += 1
-            pbar.update(1)
-
-    print(f"last.ckpt is saved at {save_dir}")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--gpu", type=int, default=0)
-    parser.add_argument("--batch_size", type=int, default=32)
-    parser.add_argument(
-        "--train_num_steps",
-        type=int,
-        default=100000,
-        help="the number of model training steps.",
-    )
-    parser.add_argument("--warmup_steps", type=int, default=500)
-    parser.add_argument("--log_interval", type=int, default=5000)
-    parser.add_argument(
-        "--max_num_images_per_cat",
-        type=int,
-        default=-1,
-        help="max number of images per category for AFHQ dataset",
-    )
-    parser.add_argument(
-        "--num_diffusion_train_timesteps",
-        type=int,
-        default=1000,
-        help="diffusion Markov chain num steps",
-    )
-    parser.add_argument("--beta_1", type=float, default=1e-4)
-    parser.add_argument("--beta_T", type=float, default=0.02)
-    parser.add_argument("--seed", type=int, default=63)
-    parser.add_argument("--image_resolution", type=int, default=32)
-
-    args = parser.parse_args()
-    main(args)
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--conditional", type=bool, default=True)
+    argparser.add_argument("--num_epochs", type=int, default=100)
+    argparser.add_argument("--scheduler", type=str, default="DDIM")
+    argparser.add_argument("--save_path", type=str, default="results/")
+    args = argparser.parse_args()
+    
+    conditional = args.conditional
+    scheduler_type = args.scheduler
+    if scheduler_type == "DDIM":
+        scheduler = DDIMScheduler(device=device)
+    elif scheduler_type == "DDPM":
+        scheduler = DDPMScheduler(device=device)
+    
+    model = UNet(device=device)
+    pipeline = DiffusionPipeline(model, scheduler)
+    dataloader = get_dataloader()
+    model = train(pipeline, dataloader)
